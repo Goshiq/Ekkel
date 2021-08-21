0f237abb432057274559d3ebc12bd75af2f13ddb
Goshiq <vangog-2005@ngs.ru>
2021-08-21 13:28:56 +0300

    4, 5 done

--- Ch16/src/_3.java
+++ Ch16/src/_3.java
@@ -2,27 +2,29 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class _3 {
-    public static Double[][] process (int fSize, int sSize, double start, double end) {
+    public static Double[][][] process (int fSize, int sSize, int tSize, double start, double end) {
         if (fSize < 0 || sSize < 0 || start > end) {
             throw new IllegalArgumentException("Wrong arguments");
         }
         Random random = new Random();
-        Double[][] result = new Double[fSize][sSize];
+        Double[][][] result = new Double[fSize][sSize][tSize];
         for (int i = 0; i < fSize; ++i) {
             for (int j = 0; j < sSize; ++j) {
-                double val = random.nextDouble();
-                int piece = random.nextInt((int)end);
-                val += piece;
-                while (!(val + piece >= start && val + piece <= end)) {
-                    piece = random.nextInt((int)end);
+                for (int k = 0; k < tSize; ++k) {
+                    double val = random.nextDouble();
+                    int piece = random.nextInt((int) end);
+                    val += piece;
+                    while (!(val + piece >= start && val + piece <= end)) {
+                        piece = random.nextInt((int) end);
+                    }
+                    result[i][j][k] = val + piece;
                 }
-                result[i][j] = val + piece;
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(process(5, 2, 4d, 5d)));
+        System.out.println(Arrays.deepToString(process(5, 2, 2, 4d, 5d)));
     }
 }
---
+++ Ch16/src/_5.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class _5 {
+    public static void main(String[] args) {
+        BerylliumSphere[][] array = new BerylliumSphere[2][2];
+        for (BerylliumSphere[] arr1: array) {
+            for (BerylliumSphere berylliumSphere: arr1) {
+                System.out.println(berylliumSphere);
+            }
+        }
+    }
+}
