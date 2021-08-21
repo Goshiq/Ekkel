cf51b98077fa4e9ff5956665c4c349f88058bd88
Goshiq <vangog-2005@ngs.ru>
2021-08-21 12:50:40 +0300

    16 init

--- Ch16/src/_1.java
+++ Ch16/src/_1.java
@@ -1,2 +1,24 @@
+class BerylliumSphere {
+    private static long count = 0;
+    private final long id = count++;
+
+    @Override
+    public String toString() {
+        return "Sphere #" + id;
+    }
+}
+
 public class _1 {
+    public static void process(BerylliumSphere[] array) {
+        for (BerylliumSphere berylliumSphere : array) {
+            System.out.println(berylliumSphere);
+        }
+    }
+
+    public static void main(String[] args) {
+        BerylliumSphere[] array = {new BerylliumSphere(), new BerylliumSphere()};
+        process(array);
+        process(new BerylliumSphere[]{new BerylliumSphere(), new BerylliumSphere()});
+//        process({new BerylliumSphere(), new BerylliumSphere()}); // does not work
+    }
 }
