c73bf44cb1948618c8475b82a8a88df5669cff66
Goshiq <vangog-2005@ngs.ru>
2021-08-22 11:23:52 +0300

    1 done

---
+++ Ch17/src/_1.java
@@ -0,0 +1,23 @@
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class _1 {
+    public static void main(String[] args) {
+        List<String> countries = new LinkedList<>();
+        countries.add("Russia");
+        countries.add("Italy");
+        countries.add("Spain");
+        countries.add("France");
+        System.out.println("Before sorting:\n" + countries);
+        Collections.sort(countries);
+        System.out.println("After sorting:\n" + countries);
+        for (int i = 0; i < 5; ++i) {
+            Collections.shuffle(countries);
+            System.out.println("After shuffling #" + (i + 1) + ":\n" + countries);
+        }
+    }
+}
