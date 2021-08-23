df520c3ec103bbef4c188c7665d70fb3f259b3fa
Goshiq <vangog-2005@ngs.ru>
2021-08-23 20:30:05 +0300

    8 done

--- Ch18/src/_7.java
+++ Ch18/src/_7.java
@@ -8,19 +8,22 @@ import java.util.List;
 
 public class _7 {
     public static void main(String[] args) {
-        try {
-            BufferedReader in = new BufferedReader(new FileReader("Ekkel.iml"));
-            List<String> list = new LinkedList<>();
-            String str;
-            while ((str = in.readLine()) != null) {
-                list.add(str);
+        if (args.length > 0) {
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(args[0])); // #8
+//            BufferedReader in = new BufferedReader(new FileReader("Ekkel.iml")); // #7
+                List<String> list = new LinkedList<>();
+                String str;
+                while ((str = in.readLine()) != null) {
+                    list.add(str);
+                }
+                Collections.reverse(list);
+                for (String s : list) {
+                    System.out.println(s);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            Collections.reverse(list);
-            for (String s: list) {
-                System.out.println(s);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
---
+++ Ch18/src/_8.java
@@ -0,0 +1,3 @@
+public class _8 {
+    // see #7
+}
